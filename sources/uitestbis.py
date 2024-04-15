f88a4e2fb48911a7f24cdf78cdc21ca6e2ee6d4b
teamcons_atwork <littlepastelworld@gmail.com>
2024-04-15 17:52:59 +0200

    f

--- sources/uitestbis.py
+++ sources/uitestbis.py
@@ -1,54 +1,54 @@
 
 #https://realpython.com/python-gui-tkinter/
 
-
-#from tkinter import *
-
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-
-from tkinter import *
-from tkinter.ttk import *
 
 
 def open_file():
     """Open a file for editing."""
-    filepath = askopenfilename(
+    filepath = tk.filedialog.askopenfilename(
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
     )
     if not filepath:
         return
-    txt_edit.delete("1.0", END)
+    txt_edit.delete("1.0", tk.END)
     with open(filepath, mode="r", encoding="utf-8") as input_file:
         text = input_file.read()
-        txt_edit.insert(END, text)
+        txt_edit.insert(tk.END, text)
     window.title(f"Simple Text Editor - {filepath}")
 
 def save_file():
     """Save the current file as a new file."""
-    filepath = asksaveasfilename(
+    filepath = tk.filedialog.asksaveasfilename(
         defaultextension=".txt",
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
     )
     if not filepath:
         return
     with open(filepath, mode="w", encoding="utf-8") as output_file:
-        text = txt_edit.get("1.0", END)
+        text = txt_edit.get("1.0", tk.END)
         output_file.write(text)
     window.title(f"Simple Text Editor - {filepath}")
 
 
 
-window = Tk()
+
+
+
+
+
+window = tk.Tk()
 window.title("Simple Text Editor")
 
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = Text(window)
-frm_buttons = Frame(window, relief=RAISED)
-btn_open = Button(frm_buttons, text="Open", command=open_file)
-btn_save = Button(frm_buttons, text="Save As...", command=save_file)
+txt_edit = tk.Text(window)
+frm_buttons = ttk.Frame(window, relief=tk.RAISED)
+btn_open = ttk.Button(frm_buttons, text="Open", command=open_file)
+btn_save = ttk.Button(frm_buttons, text="Save As...", command=save_file)
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
